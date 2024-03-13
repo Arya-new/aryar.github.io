221047f45750b1c50bf70277f38fd5ede9d188ef
Aryarmdhn <138842557+Arya-new@users.noreply.github.com>
2024-03-13 08:05:36 +0700

    add to master

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var calcDisplay = ""
+
+function btnClick(e) {
+  if (e == "1" || e == "2" || e == "3" || e == "/" || e == "*") {
+      if (e == "AC") {
+          calcDisplay = ""
+      } else {
+          calcDisplay = calcDisplay + e
+          console.log(calcDisplay)
+        } 
+        document.getElementById("display").value = calcDisplay 
+    } else {
+        console.log("Input harus berupa angka dan simbol") 
+    } 
+
+}
+      
+
